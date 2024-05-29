62946155baca84cbe6c94f10b0df33554758e74c
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2024-05-29 17:14:14 +0200

    Add homepage Call to Action

--- react/src/Homepage/Homepage.jsx
+++ react/src/Homepage/Homepage.jsx
@@ -40,6 +40,34 @@ function Homepage() {
   return (
     <>
       <Header></Header>
+
+      <main>
+        <section className="call-to-action-wrapper">
+          <div className="call-to-action">
+            <div className="call-to-action-text-wrapper">
+              <h1>Your Gateway To Gains</h1>
+              <p>
+                Your ultimate destination for comprehensive support in both
+                nutrition and workouts. Make every rep count and every movement
+                matter. Embrace the grind, challenge your limits, and push
+                yourself further.
+              </p>
+            </div>
+            <div className="call-to-action-button-wrapper">
+              <button type="button">Join Now</button>
+              <button type="button">Login</button>
+            </div>
+          </div>
+
+          <div className="call-to-action-rectangle-wrapper">
+            <div className="rectangle-1"></div>
+            <div className="rectangle-2"></div>
+            <div className="rectangle-3"></div>
+            <div className="rectangle-4"></div>
+            <div className="rectangle-5"></div>
+          </div>
+        </section>
+      </main>
     </>
   );
 }
