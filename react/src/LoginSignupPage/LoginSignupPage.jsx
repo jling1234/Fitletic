d8600f07a937c4c5d680ea03d641efdf3d96b6a3
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2024-06-24 22:13:07 +0200

    Add details to localStorage on signup

--- react/src/LoginSignupPage/LoginSignupPage.jsx
+++ react/src/LoginSignupPage/LoginSignupPage.jsx
@@ -1,13 +1,11 @@
-import {useState} from "react";
-import * as PropTypes from "prop-types";
-//import http from "../http-common";
-import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./LoginSignupPage.css";
-import {Link} from "react-router-dom";
-import {HomepageLinkLogo} from "../Shared/Logo/Logo.jsx";
+import { HomepageLinkLogo } from "../Shared/Logo/Logo.jsx";
 import axios from "axios";
+import { setUserRecord } from "../Shared/LocalDetails/LocalDetails.jsx";
+import PropTypes from "prop-types";
 
 function LoginForm() {
     const [username, setUsername] = useState("");
@@ -16,36 +14,40 @@ function LoginForm() {
     const onLoginSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.post(
-                "http://localhost:8080/signin",
-                {username: username, password: password, active: true},
-            );
+            const response = await axios.post("http://localhost:8080/signin", {
+                username: username,
+                password: password,
+                active: true,
+            });
             console.log(response.status);
 
-
-            navigate('/');
+            navigate("/");
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
-    }
-
+    };
 
     return (
         <form className="login-data-container" onSubmit={onLoginSubmit}>
             <div>
                 <p className="username-container">Username</p>
-                <input className="text-field-info" type="text" value={username}
-                       onChange={(e) => setUsername(e.target.value)}/>
+                <input
+                    className="text-field-info"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
             </div>
             <div className="password-container">
                 <p className="username">Password</p>
-                <input className="text-field-info" type="password" value={password}
-                       onChange={(e) => setPassword(e.target.value)}/>
+                <input
+                    className="text-field-info"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </div>
-            <button
-                className="login-button"
-                type="submit"
-            >
+            <button className="login-button" type="submit">
                 <p>Login</p>
             </button>
             <div className="no-account">
@@ -57,84 +59,121 @@ function LoginForm() {
     );
 }
 
-export default LoginForm;
+function SignUpForm() {
+    const [formIndex, setFormIndex] = useState(0);
 
-function SignUpForm1({onNextClick}) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+
+    const [age, setAge] = useState("");
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [activityLevel, setActivityLevel] = useState("");
+
     const navigate = useNavigate();
+
     const onSignUpSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.post(
-                "http://localhost:8080/register",
-                {username: username, password: password, active: true},
-            );
-            console.log(response.status);
-
-
-            navigate('/');
+            const response = await axios.post("http://localhost:8080/register", {
+                username: username,
+                password: password,
+                active: true,
+            });
+            setUserRecord(username, { age, height, weight, activityLevel });
+            navigate("/");
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
-    }
-    return (
-        <form className="signup-data-container" onSubmit={onSignUpSubmit}>
+    };
+
+    const forms = [
+        <form
+            key={0}
+            className="signup-data-container"
+            onSubmit={(event) => {
+                event.preventDefault();
+                setFormIndex(formIndex + 1);
+            }}
+        >
             <div className="username-container">
                 <p className="username">Username</p>
-                <input className="text-field-info" type="text"
-                       onChange={(e) => setUsername(e.target.value)}/>
+                <input
+                    className="text-field-info"
+                    type="text"
+                    onChange={(e) => setUsername(e.target.value)}
+                    value={username}
+                />
             </div>
             <div className="password-container">
                 <p className="password">Password</p>
-                <input className="text-field-info" type="password"
-                       onChange={(e) => setPassword(e.target.value)}/>
+                <input
+                    className="text-field-info"
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
             </div>
             <div className="confirm-password-container">
                 <p className="confirm-password">Confirm Password</p>
-                <input className="text-field-info" type="password"
-                       onChange={(e) => setPassword(e.target.value)}/>
+                <input
+                    className="text-field-info"
+                    type="password"
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                    value={passwordConfirm}
+                />
             </div>
-            <button className="next-button"
-                    type="button"
-                    onClick={onNextClick}>
+            <button className="next-button" type="submit">
                 <p>Next</p>
             </button>
-        </form>
-    );
-}
+        </form>,
 
-SignUpForm1.propTypes = {onNextClick: PropTypes.func};
-
-function SignUpForm2() {
-    const handleSignup = () => {
-        alert("Signup was clicked");
-    };
-
-    return (
-        <form className="signup-data-container">
+        <form key={1} className="signup-data-container" onSubmit={onSignUpSubmit}>
             <div>
                 <p className="age">Age</p>
                 <div className="age-number-field">
-                    <input type="number" min="16" className="number-field-size"/>
+                    <input
+                        type="number"
+                        min="16"
+                        className="number-field-size"
+                        value={age}
+                        onChange={(e) => setAge(e.target.value)}
+                    />
                 </div>
             </div>
             <div>
                 <p className="height">Height</p>
                 <div className="height-number-field">
-                    <input type="number" min="100" className="number-field-size"/>
+                    <input
+                        type="number"
+                        min="100"
+                        className="number-field-size"
+                        value={height}
+                        onChange={(e) => setHeight(e.target.value)}
+                    />
                 </div>
             </div>
             <div>
                 <p className="weight">Weight</p>
                 <div className="weight-number-field">
-                    <input type="number" min="30" className="number-field-size"/>
+                    <input
+                        type="number"
+                        min="30"
+                        className="number-field-size"
+                        value={weight}
+                        onChange={(e) => setWeight(e.target.value)}
+                    />
                 </div>
             </div>
             <div>
                 <p className="activity-level">Activity Level</p>
                 <div className="activity-level-select-field">
-                    <select className="dropdown">
+                    <select
+                        className="dropdown"
+                        value={activityLevel}
+                        onChange={(e) => setActivityLevel(e.target.value)}
+                    >
                         <option value="">Select</option>
                         <option value="sedentary">Sedentary</option>
                         <option value="moderately-active">Moderately Active</option>
@@ -156,26 +195,22 @@ function SignUpForm2() {
                     </select>
                 </div>
             </div>
-            <button className="signup-button" type="button" onClick={handleSignup}>
+            <button className="signup-button" type="submit">
                 <p>Sign Up</p>
             </button>
-        </form>
-    );
+        </form>,
+    ];
+
+    return forms[formIndex];
 }
 
-function LoginSignupPage({step, handleNext}) {
+function LoginSignupPage({ children }) {
     return (
         <>
             <div className="signup-page-grid-container">
                 <div className="left-grid-container">
                     <HomepageLinkLogo></HomepageLinkLogo>
-                    {step === 1 ? (
-                        <LoginForm/>
-                    ) : step === 2 ? (
-                        <SignUpForm1 onNextClick={handleNext}/>
-                    ) : (
-                        <SignUpForm2/>
-                    )}
+                    {children}
                 </div>
                 <div className="right-grid-container"></div>
             </div>
@@ -183,29 +218,20 @@ function LoginSignupPage({step, handleNext}) {
     );
 }
 
-LoginSignupPage.propTypes = {
-    step: PropTypes.number,
-    handleNext: PropTypes.func,
-    handleLoginToSignup: PropTypes.func,
-};
+LoginSignupPage.propTypes = { children: PropTypes.element.isRequired };
 
 export function LoginPage() {
-    const [step, setStep] = useState(1);
-
-    const handleNext = () => {
-        setStep((prevStep) => prevStep + 1);
-    };
-
-    return <LoginSignupPage step={step} handleNext={handleNext}/>;
+    return (
+        <LoginSignupPage>
+            <LoginForm />
+        </LoginSignupPage>
+    );
 }
 
 export function SignUpPage() {
-    const [step, setStep] = useState(2);
-
-    const handleNext = () => {
-        setStep((prevStep) => prevStep + 1);
-    };
-
-    return <LoginSignupPage step={step} handleNext={handleNext}/>;
+    return (
+        <LoginSignupPage>
+            <SignUpForm />
+        </LoginSignupPage>
+    );
 }
-
