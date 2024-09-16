6b9ccb6844a46c6c34c4ee6615aff6d6c37952b4
Midhun Pradeep Nair <52858999+midhunpradeep@users.noreply.github.com>
2024-09-16 12:27:38 +0200

    Fix invalid import

--- spring/src/main/java/com/fitletic/spring/Repository/WorkoutRepository.java
+++ spring/src/main/java/com/fitletic/spring/Repository/WorkoutRepository.java
@@ -1,12 +1,10 @@
 package com.fitletic.spring.Repository;
 
-import com.fitletic.spring.entity.UserAuthentication;
 import com.fitletic.spring.entity.WorkoutEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface WorkoutRepository extends MongoRepository<WorkoutEntity,Integer> {
